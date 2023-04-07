b2eca4dee5b6081dc86e82ebdac39b4c698d928f
Tinetsu <60636486+Tinetsu@users.noreply.github.com>
2023-04-06 19:39:25 -0700

    Copy Function work

---
+++ js/copy.js
@@ -0,0 +1,37 @@
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+function processContent(content) {
+  // Process the content according to your PRD
+  // You can create separate functions for Open Questions and Action Items
+  // This is just a placeholder example
+  return content;
+}
+
+function copyToClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+}
+
+document.getElementById('copyButton').addEventListener('click', () => {
+  const title = document.getElementById('title').value;
+  const openQuestionsContent = document.querySelector('#open-questions .ql-editor').innerHTML;
+  const actionItemsContent = document.querySelector('#action-items .ql-editor').innerHTML;
+  const mainBodyContent = document.querySelector('#main-body .ql-editor').innerHTML;
+
+  const processedOpenQuestions = processContent(openQuestionsContent);
+  const processedActionItems = processContent(actionItemsContent);
+  const processedMainBody = processContent(mainBodyContent);
+
+  const currentDate = formatDate(new Date());
+  const formattedContent = `${currentDate} - ${title}\n\nOpen Questions\n${processedOpenQuestions}\n\nAction Items\n${processedActionItems}\n\nMain Body\n${processedMainBody}`;
+  copyToClipboard(formattedContent);
+});
