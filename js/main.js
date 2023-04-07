d7f724f4d14ec11476f30d315876d78db3c9047b
Tinetsu <60636486+Tinetsu@users.noreply.github.com>
2023-04-06 19:42:13 -0700

    copy update

--- js/main.js
+++ js/main.js
@@ -1,54 +1,52 @@
-function formatDate(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
-}
-
-function processContent(content) {
-  // Process the content according to your PRD
-  // You can create separate functions for Open Questions and Action Items
-  // This is just a placeholder example
-  return content;
-}
-
-function copyToClipboard(text) {
-  const textarea = document.createElement('textarea');
-  textarea.value = text;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand('copy');
-  document.body.removeChild(textarea);
-}
-
-document.getElementById('copyButton').addEventListener('click', () => {
-  const title = document.getElementById('title').value;
-  const openQuestionsContent = document.querySelector('#open-questions .ql-editor').innerHTML;
-  const actionItemsContent = document.querySelector('#action-items .ql-editor').innerHTML;
-  const mainBodyContent = document.querySelector('#main-body .ql-editor').innerHTML;
-
-  const processedOpenQuestions = processContent(openQuestionsContent);
-  const processedActionItems = processContent(actionItemsContent);
-  const processedMainBody = processContent(mainBodyContent);
-
-  const currentDate = formatDate(new Date());
-  const formattedContent = `${currentDate} - ${title}\n\nOpen Questions\n${processedOpenQuestions}\n\nAction Items\n${processedActionItems}\n\nMain Body\n${processedMainBody}`;
-  copyToClipboard(formattedContent);
-});
-const toolbarOptions = [
-    ['bold', 'italic', 'underline'],
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-    ['link'],
-    [{ 'align': [] }],
-    [{ 'color': [] }, { 'background': [] }],
-    ['clean']
-];
-
-const quill = new Quill('#main-body', {
+document.addEventListener("DOMContentLoaded", function () {
+  const quillOpenQuestions = new Quill("#open-questions", {
     modules: {
-        toolbar: toolbarOptions
+      toolbar: "#toolbar-container",
     },
-    placeholder: 'Compose your note...',
-    theme: 'snow'
+    theme: "snow",
+  });
+
+  const quillActionItems = new Quill("#action-items", {
+    modules: {
+      toolbar: "#toolbar-container",
+    },
+    theme: "snow",
+  });
+
+  const quillMainBody = new Quill("#main-body", {
+    modules: {
+      toolbar: "#toolbar-container",
+    },
+    theme: "snow",
+  });
+
+  window.copyContent = function () {
+    const title = document.getElementById("title").value;
+    const date = new Date().toISOString().slice(0, 10);
+    const openQuestions = quillOpenQuestions.container.firstChild.innerHTML;
+    const actionItems = quillActionItems.container.firstChild.innerHTML;
+    const mainBody = quillMainBody.container.firstChild.innerHTML;
+
+    const formattedContent = `
+      ${date} - ${title}
+      
+      <h2>Open Questions</h2>
+      ${openQuestions}
+      
+      <h2>Action Items</h2>
+      ${actionItems}
+      
+      <h2>Main Body</h2>
+      ${mainBody}
+    `;
+
+    const el = document.createElement("textarea");
+    el.value = formattedContent;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+
+    alert("Content copied to clipboard!");
+  };
 });
